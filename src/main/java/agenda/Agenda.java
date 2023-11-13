89f38fb865d4e9548933c9e230d6da9c3b265a88
jihene megdiche <megdiche.jihene625@gmail.com>
2023-11-13 11:54:43 +0100

    Jihene commit

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -7,24 +7,39 @@ import java.util.*;
  * Description : An agenda that stores events
  */
 public class Agenda {
+    private ArrayList<Event> lesEvenements;
+
+    public Agenda() {
+        this.lesEvenements = new ArrayList<Event>();
+    }
+
     /**
      * Adds an event to this agenda
      *
      * @param e the event to add
      */
+
     public void addEvent(Event e) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        lesEvenements.add(e);
+    }
+
+    public void removeEvent(Event e) {
+        lesEvenements.remove(e);
     }
 
     /**
      * Computes the events that occur on a given day
      *
      * @param day the day toi test
-     * @return a list of events that occur on that day
+     * @return and iteraror to the events that occur on that day
      */
     public List<Event> eventsInDay(LocalDate day) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        ArrayList<Event> newListEvent = new ArrayList<Event>();
+        for (Event e : lesEvenements) {
+            if (e.isInDay(day)) {
+                newListEvent.add(e);
+            }
+        }
+        return newListEvent;
     }
 }
